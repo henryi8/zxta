97a464962ff0e62614776033a81b97f6f2e50436
zxy <347910189@qq.com>
2018-05-04 14:38:43 +0800

    first push~

---
+++ src/main/java/com/zxy/zxta/ZxtaApplication.java
@@ -0,0 +1,12 @@
+package com.zxy.zxta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZxtaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZxtaApplication.class, args);
+	}
+}
